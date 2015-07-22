670d24950edec259ebffaa88a4fd8d3f8d059dbb
Helge Eichhorn <git@helgeeichhorn.de>
2015-07-22 17:44:15 +0200

    Use a type instead of Dict because of v0.3 snafu.

--- test/runtests.jl
+++ test/runtests.jl
@@ -172,20 +172,25 @@ for (vf, vj) in zip(yf8, yj8)
 end
 
 # High-level interface
-function newton!(f, t, y, params)
+function newton!(f, t, y, p)
     r = sqrt(y[1]*y[1]+y[2]*y[2]+y[3]*y[3])
     r3 = r*r*r
     f[1] = y[4]
     f[2] = y[5]
     f[3] = y[6]
-    f[4] = -params["mu"]*y[1]/r3
-    f[5] = -params["mu"]*y[2]/r3
-    f[6] = -params["mu"]*y[3]/r3    
+    f[4] = -p.mu*y[1]/r3
+    f[5] = -p.mu*y[2]/r3
+    f[6] = -p.mu*y[3]/r3    
 end
-params = @compat Dict{String, Any}("mu" => mu)
+
+type Params
+    mu::Float64
+    yout::Vector{Float64}
+end
+p = Params(mu, Float64[])
 tspan = [0.0, tp]
-tj5, yj5 = dopri5(newton!, s0, tspan, points=:all, params=params)
-tj8, yj8 = dop853(newton!, s0, tspan, points=:all, params=params)
+tj5, yj5 = dopri5(newton!, s0, tspan, points=:all, params=p)
+tj8, yj8 = dop853(newton!, s0, tspan, points=:all, params=p)
 @test length(tf5) == length(tj5)
 for (a,b) in zip(tf5, tj5)
     @test_approx_eq_eps a b tol
@@ -208,8 +213,8 @@ end
 
 # Test dense output
 tspan = collect(0.0:1.0:tp)
-tj5spc, yj5spc = dopri5(newton!, s0, tspan, points=:specified, params=params)
-tj8spc, yj8spc = dop853(newton!, s0, tspan, points=:specified, params=params)
+tj5spc, yj5spc = dopri5(newton!, s0, tspan, points=:specified, params=p)
+tj8spc, yj8spc = dop853(newton!, s0, tspan, points=:specified, params=p)
 @test length(tf5spc) == length(tj5spc)
 for (a,b) in zip(tf5spc, tj5spc)
     @test_approx_eq_eps a b tol
@@ -231,8 +236,8 @@ for (vf, vj) in zip(yf8spc, yj8spc)
 end
 
 tspan = push!(collect(0.0:1.0:tp), tp)
-tj5all, yj5all = dopri5(newton!, s0, tspan, points=:all, params=params)
-tj8all, yj8all = dop853(newton!, s0, tspan, points=:all, params=params)
+tj5all, yj5all = dopri5(newton!, s0, tspan, points=:all, params=p)
+tj8all, yj8all = dop853(newton!, s0, tspan, points=:all, params=p)
 @test length(tf5all) == length(tj5all)
 for (a,b) in zip(tf5all, tj5all)
     @test_approx_eq_eps a b tol
@@ -255,25 +260,23 @@ end
 
 # Test solout interface
 function solout!(told, t, y, contd, params)
-    yout = zeros(Float64, 7)
     if told < 5000 < t
-        yout[1] = 5000.0
+        push!(params.yout, 5000.0)
         for i = 1:6
-            yout[i+1] = contd(i, 5000)
+            push!(params.yout, contd(i, 5000))
         end
-        d = @compat Dict{String, Any}("yout"=>yout)
-        merge!(params, d)
         return dopricode[:abort]
     else
         return dopricode[:nominal]
     end
 end
-tj5, yj5 = dopri5(newton!, s0, tspan, solout=solout!, params=params)
-for (a,b) in zip([tf5spc[5001]; yf5spc[5001]], params["yout"])
+tj5, yj5 = dopri5(newton!, s0, tspan, solout=solout!, params=p)
+for (a,b) in zip([tf5spc[5001]; yf5spc[5001]], p.yout)
     @test_approx_eq_eps a b tol
 end
-tj8, yj8 = dop853(newton!, s0, tspan, solout=solout!, params=params)
-for (a,b) in zip([tf8spc[5001]; yf8spc[5001]], params["yout"])
+p = Params(mu, Float64[])
+tj8, yj8 = dop853(newton!, s0, tspan, solout=solout!, params=p)
+for (a,b) in zip([tf8spc[5001]; yf8spc[5001]], p.yout)
     @test_approx_eq_eps a b tol
 end
 
