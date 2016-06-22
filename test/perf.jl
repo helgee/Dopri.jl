58834a959acbe29cbe48867f4082939e2b5eca95
Helge Eichhorn <git@helgeeichhorn.de>
2016-06-22 09:48:38 -0400

    Update perf script.

--- test/perf.jl
+++ test/perf.jl
@@ -15,11 +15,11 @@ tspan = [0.0, 5402.582703094263]
 mu = 398600.4415
 s0 = [-1814.0, -3708.0, 5153.0, 6.512, -4.229, -0.744]
 
-b5 = @benchmark dopri5(newton!, s0, tspan, points=:last, params=mu)
-b8 = @benchmark dop853(newton!, s0, tspan, points=:last, params=mu)
+b5 = @benchmark dopri5((f, t, y) -> newton!(f, t, y, mu), s0, tspan, points=:last)
+b8 = @benchmark dop853((f, t, y) -> newton!(f, t, y, mu), s0, tspan, points=:last)
 println("DOPRI5\n$b5")
 println("DOP853\n$b8")
 
 n = 1_00_000
-@profile for i=1:n; dop853(newton!, s0, tspan, points=:last, params=mu); end
+@profile for i=1:n; dop853((f, t, y) -> newton!(f, t, y, mu), s0, tspan, points=:last); end
 Profile.print()
