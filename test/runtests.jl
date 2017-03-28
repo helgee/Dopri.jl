8ddd004077abfaddf40ebe93a8464e5978c00c82
Helge Eichhorn <git@helgeeichhorn.de>
2017-03-28 11:03:17 +0200

    Drop 0.4 and fix OSX Travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,4 @@
-if VERSION >= v"0.5.0-dev+7720"
-    using Base.Test
-else
-    using BaseTestNext
-    const Test = BaseTestNext
-end
-using Compat
+using Base.Test
 using Dopri
 
 include("fortran.jl")
